75052a673e8c5e805fd2e8394071ce3f05f68392
smadduff <juanpablocampinotoro@gmail.com>
2023-06-22 23:13:01 -0400

    arreglo error

--- core/context_processors.py
+++ core/context_processors.py
@@ -16,16 +16,20 @@ def registrar_carrito(request):
     }
 
 def carrito_variables(request):
-    detalle_carrito = Carrito.objects.filter(cliente=request.user.perfil)
-    total_a_pagar = 0
-    for item in detalle_carrito:
-        total_a_pagar += item.precio_a_pagar
-    monto_sin_iva = int(round(total_a_pagar / 1.19))
-    iva = total_a_pagar - monto_sin_iva
+    if request.user.is_authenticated and hasattr(request.user, 'perfil') and request.user.perfil.tipo_usuario == 'Cliente':
+        detalle_carrito = Carrito.objects.filter(cliente=request.user.perfil)
+        total_a_pagar = 0
+        for item in detalle_carrito:
+            total_a_pagar += item.precio_a_pagar
+        monto_sin_iva = int(round(total_a_pagar / 1.19))
+        iva = total_a_pagar - monto_sin_iva
 
-    return {
-        'detalle_carrito': detalle_carrito,
-        'monto_sin_iva': monto_sin_iva,
-        'iva': iva,
-        'total_a_pagar': total_a_pagar,
-    }
+        return {
+            'detalle_carrito': detalle_carrito,
+            'monto_sin_iva': monto_sin_iva,
+            'iva': iva,
+            'total_a_pagar': total_a_pagar,
+        }
+    
+    # En caso de que las condiciones anteriores no se cumplan, puedes devolver un diccionario vacío o None.
+    return {}
